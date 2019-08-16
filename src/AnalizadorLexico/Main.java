1b373bfa1e906c9d7fd98fa4fc9743c400644a48
Carlos Morales <carlosmorales1284@hotmail.com>
2019-08-15 18:24:32 -0600

    commit Lexer.flex
    Creación e implementación de Lexer.flex

---
+++ src/AnalizadorLexico/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalizadorLexico;
+
+import java.io.File;
+
+/**
+ *
+ * @author carlo
+ */
+public class Main {
+    public static void main(String[] args) {
+        String ruta = "C:/Users/carlo/Documents/NetBeansProjects/miniSQL/src/AnalizadorLexico/Lexer.flex";
+        generarLexema(ruta);
+    }
+    
+    public static void generarLexema(String ruta){
+        File file = new File (ruta);
+        jflex.Main.generate(file);
+    }
+}
---
+++ src/AnalizadorLexico/Tokens.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AnalizadorLexico;
+
+/**
+ *
+ * @author carlo
+ */
+public enum Tokens {
+    Reservada,
+    Operador,
+    Identificador,
+    Numero,
+    Error
+}
