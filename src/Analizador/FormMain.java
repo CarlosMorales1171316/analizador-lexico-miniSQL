0b2ffe0e8e534cf813e97227eb3e61bad8248f37
Carlos Morales <carlosmorales1284@hotmail.com>
2019-10-30 00:15:46 -0600

    commit precedencia CUP
    mejorar errores de shif/reduce

--- src/Analizador/FormMain.java
+++ src/Analizador/FormMain.java
@@ -918,7 +918,7 @@ public class FormMain extends javax.swing.JFrame {
         s.parse();
         
         //Lista de errores sintácticos
-        ErroresSintacticos = s.erroresSintacticos;
+        ErroresSintacticos = s.SyntacticErrors;
         
         for(String element: ErroresSintacticos){
                 if(Contenido.contains(element) == false)
